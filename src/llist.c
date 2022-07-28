de5c5724abd66ce653f9646d09f7f4ec8760322f
anaximeno <anaximenobrito@gmail.com>
2022-07-28 16:14:01 -0100

    refactor: abstracted the list node search by index algorithm

--- src/llist.c
+++ src/llist.c
@@ -68,6 +68,39 @@ static int get_index_of_value(l_list* self, value_t value)
 }
 
 
+/* Returns a node at the given index. */
+static _l_node* get_node_at_index(l_list* list, unsigned index)
+{
+    if (list != NULL && index < list->length) {
+        _l_node* node = NULL;
+        unsigned int i = 0;
+        float mid_index = (float) list->length / 2;
+
+        if (index < mid_index) {
+            /* Forward search */
+            i = 0;
+            node = &list->head;
+            while (i < index && *node != NULL) {
+                node = &(*node)->next;
+                i += 1;
+            }
+        } else {
+            /* Backward search */
+            i = list->length - 1;
+            node = &list->tail;
+            while (i > index && *node != NULL) {
+                node = &(*node)->prev;
+                i -= 1;
+            }
+        }
+
+        return node;
+    }
+
+    return NULL;
+}
+
+
 /* Returns the value at the given index. Zero will be returned
  * by default for out of bound indexes.
  * */
@@ -76,29 +109,10 @@ static value_t get_value_at_index(l_list* self, unsigned idx)
 #define DEFAULT_RETURN_TYPE 0
 
     if (self != NULL && idx < self->length) {
-        _l_node node = NULL;
-        unsigned int i = 0;
-        float mid_idx = (float) self->length / 2;
+        _l_node* node = get_node_at_index(self, idx);
 
-        if (idx < mid_idx) {
-            /* Forward search */
-            i = 0;
-            node = self->head;
-            while (i < idx && node != NULL) {
-                node = node->next;
-                i += 1;
-            }
-        } else {
-            /* Backward search */
-            i = self->length - 1;
-            node = self->tail;
-            while (i > idx && node != NULL) {
-                node = node->prev;
-                i -= 1;
-            }
-        }
-
-        return node != NULL ? node->value : DEFAULT_RETURN_TYPE;
+        if (*node != NULL)
+            return (*node)->value;
     }
 
     return DEFAULT_RETURN_TYPE;
