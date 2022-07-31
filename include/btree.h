4813e00c6713e58a385ce8c07e6e3a471a2ae904
anaximeno <anaximenobrito@gmail.com>
2022-07-31 14:59:59 -0100

    feat: add some methods to be implemented

--- include/btree.h
+++ include/btree.h
@@ -20,9 +20,17 @@ enum BinNodeRepetitionStrategy {
     IGNORE
 };
 
+enum BinaryThreePrintOrder {
+    IN_ORDER,
+    PRE_ORDER,
+    POST_ORDER
+};
+
 typedef struct binarytree b_tree;
-typedef enum BinNodePosition b_node_pos;
-typedef enum BinNodeRepetitionStrategy b_node_rep_st;
+
+typedef enum BinNodePosition                b_node_pos;
+typedef enum BinNodeRepetitionStrategy      b_node_rep_st;
+typedef enum BinaryThreePrintOrder          b_tree_print_ord;
 
 
 /* Represents a unique node of the binary tree. */
@@ -30,7 +38,7 @@ struct _binary_node {
     struct _binary_node* parent;
     struct _binary_node* left;
     struct _binary_node* right;
-    b_node_pos relative_positioning;
+    b_node_pos rel_pos;
     value_t value;
     unsigned level;
 };
@@ -42,7 +50,34 @@ struct binarytree {
     b_node_rep_st rep_strategy;
     unsigned depth;
 
+    /* Adds a new value to the binary tree. */
     void (*add) (b_tree* self, value_t value);
+
+    /* Searchs for the value on the tree and returns the level in
+     * which it was firstly found. If the value is not inside the tree,
+     * the constant `VALUE_NOT_FOUND` is returned.
+     * */
+    int (*search) (b_tree* self, value_t value);
+
+    /* Searchs for a value, returning the level it was first found, after
+     * the given level.
+     * */
+    int (*search_after_level) (b_tree* self, value_t value, unsigned level);
+
+    /* Removes the given value completely from the b_tree.
+     * If the repetition strategy chosen was APPEND_(LEFT/RIGTH),
+     * other repetitions will also be removed.
+     * */
+    void (*remove) (b_tree* self, value_t value);
+
+    /* Removes the given value if it is in the given level of the tree. */
+    void (*remove_at_level) (b_tree* self, value_t value, unsigned level);
+
+    /* Prints the binary tree in the order determined by the param ord. */
+    void (*print) (b_tree_print_ord ord);
+
+    /* Balances the binary tree using the AVL tree balancing algorithm. */
+    void (*balance) (b_tree* self);
 };
 
 
