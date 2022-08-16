ab9962ad5b2774d722301ec821ec7fd0af7ae765
anaximeno <anaximenobrito@gmail.com>
2022-08-16 00:39:16 -0100

    fix: add type when creating a new value_t using the functs

--- src/core.c
+++ src/core.c
@@ -13,37 +13,37 @@
 
 extern value_t new_int_value_t(int32_t value)
 {
-    WRAP_AROUND(value_wrapper->data.integer = value;);
+    WRAP_AROUND(value_wrapper->data.integer = value; value_wrapper->type = INTEGER;);
 }
 
 extern value_t new_uint_value_t(uint32_t value)
 {
-    WRAP_AROUND(value_wrapper->data.uinteger = value;);
+    WRAP_AROUND(value_wrapper->data.uinteger = value; value_wrapper->type = UNSIGNED;);
 }
 
 extern value_t new_float32_value_t(float value)
 {
-    WRAP_AROUND(value_wrapper->data.float32 = value;);
+    WRAP_AROUND(value_wrapper->data.float32 = value; value_wrapper->type = FLOAT32;);
 }
 
 extern value_t new_float64_value_t(double value)
 {
-    WRAP_AROUND(value_wrapper->data.float64 = value;);
+    WRAP_AROUND(value_wrapper->data.float64 = value; value_wrapper->type = FLOAT64;);
 }
 
 extern value_t new_string_value_t(char* value)
 {
-    WRAP_AROUND(value_wrapper->data.string = value;);
+    WRAP_AROUND(value_wrapper->data.string = value; value_wrapper->type = STRING;);
 }
 
 extern value_t new_char_value_t(char value)
 {
-    WRAP_AROUND(value_wrapper->data.character = value;);
+    WRAP_AROUND(value_wrapper->data.character = value; value_wrapper->type = CHARACTER;);
 }
 
 extern value_t new_pointer_value_t(void* value)
 {
-    WRAP_AROUND(value_wrapper->data.pointer = value;);
+    WRAP_AROUND(value_wrapper->data.pointer = value; value_wrapper->type = POINTER;);
 }
 
 extern char* value_t_repr(value_t value)
