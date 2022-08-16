0d3f64e99dc50a1d8e339cca903d3050cdd980a7
anaximeno <anaximenobrito@gmail.com>
2022-08-16 00:28:07 -0100

    feat: update value_t data creation

--- include/core.h
+++ include/core.h
@@ -1,6 +1,8 @@
 #ifndef _3S_CORE_HEADER
 #define _3S_CORE_HEADER
 
+#include <stdint.h>
+
 /* Returned when searching for
  * values on lists or arrays.
  * This is defined on the
@@ -15,28 +17,76 @@
  * */
 #define _MAKE_ROBUST_CHECK
 
-/* General type of the values on the
- * trees and linked lists. This is defined
- * on the core module.
- * */
-typedef int value_t;
+/* The types of values allowed inside the value_t wrapper. */
+typedef enum __value_type_enumerated_types {
+    INTEGER, UNSIGNED, FLOAT32, FLOAT64,
+    STRING, CHARACTER, POINTER
+} value_t_types;
 
-/* This is the default formatter of the
- * type above. It will be used in situations
- * that the code must be printed through this library.
- * Please remember to change the format accordingly to
- * the type of value defined above.
+
+/* Wrapper used to store the actual data inside.
+ * It was made so it could allow different types
+ * to be stored in a shared space.
  * */
-#define __VALUE_T_DEFAULT_PRINTF_FORMAT "%d"
+typedef struct value_t {
+    /* This union stores the actual data
+     * inside one of its fields.
+     * */
+    union value_data_type {
+        int32_t integer;
+        uint32_t uinteger;
+        float float32;
+        double float64;
+        char* string;
+        char character;
+        void* pointer;
+    } data;
+    /* This represents the type of the
+     * data being stored on the specific instance
+     * of this structure.
+     * */
+    value_t_types type;
+} value_t;
+
+/* Returns a newly allocated value_t of type INTEGER */
+extern value_t* new_int_value_t(int32_t);
+/* Returns a newly allocated value_t of type UNSIGNED */
+extern value_t* new_uint_value_t(uint32_t);
+/* Returns a newly allocated value_t of type FLOAT32 */
+extern value_t* new_float32_value_t(float);
+/* Returns a newly allocated value_t of type FLOAT64 */
+extern value_t* new_float64_value_t(double);
+/* Returns a newly allocated value_t of type STRING */
+extern value_t* new_string_value_t(char*);
+/* Returns a newly allocated value_t of type CHARACTER */
+extern value_t* new_char_value_t(char);
+/* Returns a newly allocated value_t of type POINTER */
+extern value_t* new_pointer_value_t(void*);
+
+/* When in use, a global struct named value_factory will be available. */
+#define USE_VALUE_FACTORY\
+    const struct __value_t_factory {\
+        value_t* (*new_int) (int32_t);\
+        value_t* (*new_uint) (uint32_t);\
+        value_t* (*new_float32) (float);\
+        value_t* (*new_float64) (double);\
+        value_t* (*new_string) (char*);\
+        value_t* (*new_char) (char);\
+        value_t* (*new_pointer) (void*);\
+    } value_factory = {\
+        .new_int=&new_int_value_t,\
+        .new_uint=&new_uint_value_t,\
+        .new_float32=&new_float32_value_t,\
+        .new_float64=&new_float64_value_t,\
+        .new_string=&new_string_value_t,\
+        .new_char=&new_char_value_t,\
+        .new_pointer=&new_pointer_value_t,\
+    };
 
 /* Maximum size of the string representation of the value_t. */
 #define __VALUE_T_REPR_BUFFER_MAX_SIZE 30
 
 /* Converts the value to its repr, returning the value stored into the given buffer. */
-extern char* value_t_repr(const char* default_format,
-                          const unsigned max_buf_size,
-                          value_t value);
-
-#define VALUE_T_REPR(value) value_t_repr(__VALUE_T_DEFAULT_PRINTF_FORMAT, __VALUE_T_REPR_BUFFER_MAX_SIZE, value)
+extern char* value_t_repr(value_t value);
 
 #endif /* _3S_CORE_HEADER */
--- src/core.c
+++ src/core.c
@@ -4,11 +4,86 @@
 #include <string.h>
 #include <stdio.h>
 
-extern char* value_t_repr(const char* default_format,
-                          const unsigned max_buf_size,
-                          value_t value)
+#define WRAP_AROUND(STTMT) ({\
+    value_t* value_wrapper = (value_t*) malloc(sizeof(value_t));\
+    if (value_wrapper != NULL) { STTMT }\
+    return value_wrapper;\
+})
+
+
+extern value_t* new_int_value_t(int32_t value)
 {
-    char* buffer = (char*) malloc(max_buf_size * sizeof(char));
-    sprintf(buffer, default_format, value);
+    WRAP_AROUND(value_wrapper->data.integer = value;);
+}
+
+extern value_t* new_uint_value_t(uint32_t value)
+{
+    WRAP_AROUND(value_wrapper->data.uinteger = value;);
+}
+
+extern value_t* new_float32_value_t(float value)
+{
+    WRAP_AROUND(value_wrapper->data.float32 = value;);
+}
+
+extern value_t* new_float64_value_t(double value)
+{
+    WRAP_AROUND(value_wrapper->data.float64 = value;);
+}
+
+extern value_t* new_string_value_t(char* value)
+{
+    WRAP_AROUND(value_wrapper->data.string = value;);
+}
+
+extern value_t* new_char_value_t(char value)
+{
+    WRAP_AROUND(value_wrapper->data.character = value;);
+}
+
+extern value_t* new_pointer_value_t(void* value)
+{
+    WRAP_AROUND(value_wrapper->data.pointer = value;);
+}
+
+extern char* value_t_repr(value_t value)
+{
+    char* buffer = (char*) malloc(__VALUE_T_REPR_BUFFER_MAX_SIZE * sizeof(char));
+
+    switch (value.type)
+    {
+    case INTEGER:
+        sprintf(buffer, "%d", value.data.integer);
+        break;
+
+    case UNSIGNED:
+        sprintf(buffer, "%u", value.data.uinteger);
+        break;
+
+    case FLOAT32:
+        sprintf(buffer, "%f", value.data.float32);
+        break;
+
+    case FLOAT64:
+        sprintf(buffer, "%lf", value.data.float64);
+        break;
+
+    case STRING:
+        sprintf(buffer, "%s", value.data.string);
+        break;
+
+    case CHARACTER:
+        sprintf(buffer, "%c", value.data.character);
+        break;
+
+    case POINTER:
+        sprintf(buffer, "&[%p]", value.data.pointer);
+        break;
+
+    default:
+        // TODO: handle here, exit gracefully?
+        break;
+    }
+
     return buffer;
 }
